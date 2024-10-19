e851d2b609b900a04cc62ba4af44959b6211c9ae
bhogenikita1 <167703036+bhogenikita1@users.noreply.github.com>
2024-10-20 00:13:01 +0530

    Update advanced_python.py

--- 01-Python/advanced_python.py
+++ 01-Python/advanced_python.py
@@ -1,280 +1,165 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 19 23:55:48 2024
-
-@author: HP
-"""
-
 ##############   ADVANCED PYTHON   ################
-        
-#### list comprehension ####
 
-list=[]
-for num in range(0,20):
-    list.append(num)
-print(list)
+#### List Comprehension ####
 
-#######################
+# Basic list generation using a for loop
+basic_list = []
+for num in range(0, 20):
+    basic_list.append(num)
+print(basic_list)
 
-##by list comprehension
-list=[num for num in range(0,20)]
-print(list)
+# Using list comprehension for the same task
+comprehension_list = [num for num in range(0, 20)]
+print(comprehension_list)
 
-#######################
+# Capitalizing names in a list using list comprehension
+names = ["dada", "mama", "kaka"]
+capitalized_names = [name.capitalize() for name in names]
+print(capitalized_names)
 
-names=["dada","mama","kaka"]
-list=[name.capitalize() for name in names]
-print(list)
-
-#######################
-
-##comprehension with if statement
+# List comprehension with if statement (filtering even numbers)
 def is_even(num):
-    return num%2==0
-list=[num for num in range(21) if is_even(num)]
-print(list)
+    return num % 2 == 0
 
-##comphrension with nested for loop 
-list=[f"{x}{y}"for x in range(3) for y in range(3)]
-print(list)
+even_list = [num for num in range(21) if is_even(num)]
+print(even_list)
 
-##dict comprehension
-dict={x:x*x for x in range(3)}
-print(dict)
+# List comprehension with nested for loops
+nested_list = [f"{x}{y}" for x in range(3) for y in range(3)]
+print(nested_list)
 
-##Generator
-'''It is another way of creating iterators in a simple way
-where it uses the key word "yield".Instead of returning
-it in a defined function.
-Generators are implemented using a function'''
+#### Dictionary Comprehension ####
 
-##generators is a function that returns an iterator using the yeild keyword.
-#it is normal function  but whenever it needs to genarate a value, 
-#it uses "yeild" keyword instead of "return" keyword
-#if a body of a def fun contain yeild keyword it automatically
-# becomes a python generator function 
+# Creating a dictionary where keys are numbers and values are their squares
+squares_dict = {x: x*x for x in range(3)}
+print(squares_dict)
 
-##generator for all values
-gen=(x for x in range(3))
-print(gen)
-for num in gen:
-    print(num)
-    
-############################################
+#### Generators ####
 
-#generator for single values
-gen=(x for x in range(3))
-next(gen)
-next(gen)
-
-#####################################
-
-##functions which return multiple values
+# Simple generator for numbers using the 'yield' keyword
 def range_even(end):
-    for num in range(0,end,2):
+    for num in range(0, end, 2):
         yield num
-for num in range_even(6):
-    print(num)
-    
-###########################################################   
- 
-#now instead of usimg for loop we can write create generators
-gen=range_even(6)
-next(gen)
-next(gen)
-    
-###chaining generators
+
+# Accessing generator values with 'next()'
+even_gen = range_even(6)
+print(next(even_gen))  # 0
+print(next(even_gen))  # 2
+print(next(even_gen))  # 4
+
+# Generator for creating hidden password
 def lengths(itr):
     for ele in itr:
         yield len(ele)
+
 def hide(itr):
     for ele in itr:
-        yield ele*"*"
-        2
-password=["not good","give m-pass"]    
-for password in hide(lengths(password)):
-    print(password)
-    
-        
-'''
-"ele*" appears to be a placeholder for an element
-from an iterable.The asterisk(*) is likely just a character
-used to represent a placeholder or a wildcard.
-for instance if u r iterating over a list of elemets,"ele*"
-could symbolize any representation that does not  correspond to any spevific syntax
-in python or itertolls'''
+        yield ele * "*"
 
+passwords = ["not good", "give m-pass"]
+for hidden in hide(lengths(passwords)):
+    print(hidden)
 
-##take password from user and hide it
-adj=input("Enter an adj:")
-noun=input("Enter a noun:")
-number=input("Enter a number:")
-sc=input("Enter a special character:")
-password=adj+noun+str(number) +sc
-print("Your password is: %s"%password) 
-def lengths(itr):
-    for ele in itr:
-        yield len(ele)
-def hide(itr):
-    for ele in itr:
-        yield ele* "*"
-password=adj+noun+str(number)+sc    
-for password in hide(lengths(password)):
-    print(password,end="") 
-    
-################### 25/03/24 ###################### 
-  
-####    Enumerator
+#### Create and Hide Password from User Input ####
 
-## printing list with index
-list=["Apple","Mango","Wmelon"]
-for index in range(len(list)):
-    print(f"{index+1}-{list[index]}")
- 
-#same code can be implemented using enumerate
-list=["Apple","Mango","Wmelon"]
-for index,item in enumerate(list,start=1):
-    print(f"{item}-{index}")    
-    
-#use of zip function    
-list1=["Dada","Mama","Kaka"]
-mob=["9975","8648","9327"]
-for nm,li  in zip(mob,list1):
-        print(f"{nm}-{li}")    
-    
-#use of zip function with mis match list
+adj = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+number = input("Enter a number: ")
+sc = input("Enter a special character: ")
+user_password = adj + noun + str(number) + sc
+print(f"Your password is: {user_password}")
+
+# Hiding the generated password
+hidden_password = hide(lengths(user_password))
+for hidden_char in hidden_password:
+    print(hidden_char, end="")
+
+#### Enumerators and Zipping ####
+
+# Printing list with index using a for loop
+fruits = ["Apple", "Mango", "Watermelon"]
+for index in range(len(fruits)):
+    print(f"{index+1} - {fruits[index]}")
+
+# Using enumerate to simplify the above task
+for index, fruit in enumerate(fruits, start=1):
+    print(f"{fruit} - {index}")
+
+# Using zip function to combine two lists
+names = ["Dada", "Mama", "Kaka"]
+mobiles = ["9975", "8648", "9327"]
+for name, mobile in zip(mobiles, names):
+    print(f"{mobile} - {name}")
+
+# Using zip_longest to handle mismatched lists
 from itertools import zip_longest
-list1=["Dada","Mama","Kaka","Baba"]
-mob=["9975","8648","9327"]
-for nm,li  in zip_longest(mob,list1):
-    print(f"{nm}-{li}") 
+for name, mobile in zip_longest(mobiles, names, fillvalue="Unknown"):
+    print(f"{mobile} - {name}")
 
-##use of fill value instead none
-from itertools import zip_longest
-list1=["Dada","Mama","Kaka","Baba"]
-mob=["9975","8648","9327"]
-for nm,li  in zip_longest(mob,list1,fillvalue=0):
-    print(f"{nm}-{li}") 
+#### Useful Functions: all(), any(), count(), cycle() ####
 
-###############################
-
-#use of all(),if all the values are true then it will
-#produce output  
-list=[2,3,4,1,-2,0,-4]#values must be non zero,+ve,-ve
-if all(list):
-    print("all values are true")
+# all() example: returns True if all values are non-zero
+values = [2, 3, 4, 1, -2, 0, -4]
+if all(values):
+    print("All values are true")
 else:
-    print("there are non zero values")
-#O/P->all values are true 
+    print("There are zero values")
 
-###############
-   
-list=[2,3,4,1,0,-2,7,-4]#values must be non zero,+ve,-ve
-if all(list):
-    print("all values are true")
+# any() example: returns True if at least one value is non-zero
+if any(values):
+    print("There are some non-zero values")
 else:
-    print("there are non zero values")
-#O/P->there are non zero values        
-    
-######################################
+    print("All values are zero")
 
-#use of any(),if any one non zero values
-list=[2,3,4,1,0,-2,0,7,-4]
-if any(list):
-    print("it has some non zero values")
-else:
-    print("useless")
-#O/P->it has some non zero values
-
-##################        
-
-list=[0,0,0,0,0,0]
-if any(list):
-    print("it has some non zero values")
-else:
-    print("useless,all values are null")    
-#O/P->useless,all values are null
-
-######################################
-    
-##count()
+# count(): generates an infinite sequence starting from 0 or a given number
 from itertools import count
-counter=count()
-print(next(counter))#starts from 0
-print(next(counter))
-print(next(counter))
-print(next(counter))
-print(next(counter))
+counter = count(start=1)
+print(next(counter))  # 1
+print(next(counter))  # 2
+print(next(counter))  # 3
 
+# cycle(): repeats an iterable infinitely
+from itertools import cycle
+tasks = ["eat", "code", "sleep", "repeat"]
+for task in cycle(tasks):
+    print(task)
 
-####################
-
-#now let us start from 1
-from itertools import count
-counter=count(start=1)
-print(next(counter))#starts from 1
-print(next(counter))
-print(next(counter))
-
-###############################
-
-#cycle()
-#suppose you have repeated tasks to be done,then
-import itertools
-instruct=("eat","code","sleep","repeat")
-for instr  in itertools.cycle(instruct):
-    print(instr)
-
-##repeat()
+# repeat(): repeats a single value for a specified number of times
 from itertools import repeat
-for msg in repeat("keep patience",times=20):
+for msg in repeat("Keep patience", times=3):
     print(msg)
-    
-###combination()
+
+#### Combinations and Permutations ####
+
+# Combinations: select items without regard to order
 from itertools import combinations
-players=["Virat","Dhoni","Rohit","Sachin"]
-for i in combinations(players,2):
-    print(i)
-    
-#permutations()
+players = ["Virat", "Dhoni", "Rohit", "Sachin"]
+for combo in combinations(players, 2):
+    print(combo)
+
+# Permutations: select items with regard to order
 from itertools import permutations
-players=["Virat","Dhoni","Rohit"]
-for i in permutations(players,4):
-      print(i) 
-    
-#product()
-from itertools import product
-team_a=["Virat","Dhoni","Rohit"]
-team_b=["Shami","Bumrah","Ashwin"]
-for pair in product(team_a,team_b):
-    print(pair)
-    
+for perm in permutations(players, 3):
+    print(perm)
 
-###shallow and deep copy
-'''In python assignment statement (a=b) do not create
-real copies
-it only creates a new variable with same reference.
-so when u want to make actual copies of mutable objects
-(list,dicts)
-and want to modify the copy without affecting the original
-you have to be careful.
-for "real"copies we can use the copies module.
-however,for compound/nested objects (eg nested lists or dicts) and
-custom objects there is an imp dufference between shallow
-and deep copying.
+#### Shallow and Deep Copying ####
 
-shallow copy->only one level deep.it creates a new 
-copy and populates it with references to the nested objects.
-this means modyfying a nested obj in the copy deeper than modification in original also
-deep copy->a full independent clone.it creates a new 
-copy and then recusively populates it with copies of 
-nested'''
+# Assignment operation (shallow copy)
+list_a = [1, 2, 3, 4]
+list_b = list_a  # Both point to the same memory location
+list_a[0] = -10
+print(list_a)  # [-10, 2, 3, 4]
+print(list_b)  # [-10, 2, 3, 4] (same as list_a)
 
-#assignment operation
-#This will create a new variable with the same reference
-list_a=[1,2,3,4,2]
-list_b=list_a
-list_a[0]=-10
-print(list_a) 
-print(list_b)    
-    
+# Shallow copy using list slicing
+import copy
+list_c = copy.copy(list_a)
+list_c[0] = 100
+print(list_c)  # [100, 2, 3, 4] (independent from list_a)
+
+# Deep copy (creates an independent copy)
+nested_list = [[1, 2], [3, 4]]
+deep_copy_list = copy.deepcopy(nested_list)
+nested_list[0][0] = 99
+print(nested_list)  # [[99, 2], [3, 4]]
+print(deep_copy_list)  # [[1, 2], [3, 4]] (independent from nested_list)
